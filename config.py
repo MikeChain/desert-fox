dee131f63288344b772747a8c9b440776c56f537
Miguel Ángel Cadena Sánchez <miguelangel_cadena@outlook.com>
2023-02-10 07:49:27 -0600

    Se agregan nuevas configuraciones de flask

--- config.py
+++ config.py
@@ -13,3 +13,10 @@ class Config:
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = os.environ.get("FLASK_DEBUG", default=False)
+    PROPAGATE_EXCEPTIONS = True
+    API_TITLE = "Desert fox REST API"
+    API_VERSION = "v1"
+    OPENAPI_VERSION = "3.0.3"
+    OPENAPI_URL_PREFIX = "/"
+    OPENAPI_SWAGGER_UI_PATH = "/swagger-ui"
+    OPENAPI_SWAGGER_UI_URL = "https:cdn.jsdelivr.net/npm/swagger-ui-dist/"
