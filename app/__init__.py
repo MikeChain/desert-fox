0d2d304573b192eab077ad1745a9df57d2963c19
Miguel Ángel Cadena Sánchez <miguelangel_cadena@outlook.com>
2023-02-10 07:58:49 -0600

    Se agrega smorest y migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask_smorest import Api
+from flask_migrate import Migrate
+
 from config import Config
 from app.extensions import db
+
+BPS_TO_IMPORT = ()
 
 
 def create_app():
@@ -10,8 +15,14 @@ def create_app():
 
     db.init_app(app)
 
+    migrate = Migrate(app, db)
+    api = Api(app)
+
     @app.route("/test/")
     def test_page():
         return "<h1>Probando el patrón de fábrica de aplicaciones Flask</h1>"
+
+    for bp in BPS_TO_IMPORT:
+        api.register_blueprint(bp)
 
     return app
