ab68902a2354dbfef330d6247aade14125a885aa
Miguel Ángel Cadena Sánchez <miguelangel_cadena@outlook.com>
2023-02-12 15:33:59 -0600

    se refactoriza rutas de usuario para usar el servicio

--- app/routes/user.py
+++ app/routes/user.py
@@ -6,6 +6,7 @@ from sqlalchemy.exc import SQLAlchemyError
 from app.extensions import db
 from app.models import UserModel
 from app.schemas import UserSchema, UserUpdateSchema
+from app.services import UserService
 
 bp = Blueprint(
     "Users", __name__, description="Operations on users", url_prefix="/users"
@@ -15,24 +16,20 @@ bp = Blueprint(
 @bp.route("/")
 class UsersList(MethodView):
     @bp.response(200, UserSchema(many=True))
-    def get(self) -> UserModel:
-        return UserModel.query.all()
+    def get(self):
+        return UserService.get_all_users()
 
 
 @bp.route("/<uuid:user_id>")
 class User(MethodView):
-    @staticmethod
-    def get_user_by_id(user_id) -> UserModel:
-        return UserModel.query.get_or_404(user_id)
-
     @bp.response(200, UserSchema)
     def get(self, user_id):
-        return self.get_user_by_id(user_id)
+        return UserService.get_user_404(user_id)
 
     @bp.arguments(UserUpdateSchema)
     @bp.response(200, UserSchema)
     def put(self, user_data, user_id):
-        user = self.get_user_by_id(user_id)
+        user = UserService.get_user_404(user_id)
 
         user.display_name = user_data.get("display_name", user.display_name)
         user.email = user_data.get("email", user.email)
