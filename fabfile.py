00900606b0c1cb89f6924e196d99a927f7bb24e0
Miguel Ángel Cadena Sánchez <miguelangel_cadena@outlook.com>
2023-02-26 20:33:30 -0600

    Se agregan capacidades para hacer deploy y consultar logs de error y acceso al servidor remoto

---
+++ fabfile.py
@@ -0,0 +1,33 @@
+import os
+
+from fabric import Connection, task
+
+
+@task
+def deploy(c: Connection):
+    with c.cd("~/proyecto/desert-fox"):
+        c.run("git pull")
+
+        with c.prefix("source env/bin/activate"):
+            c.run("python -m pip install --upgrade pip")
+
+            c.run("flask --app main db upgrade")
+
+    c.sudo("systemctl restart desertfox")
+    c.sudo("systemctl restart nginx")
+
+    print("Deploy completo")
+
+
+@task
+def error(c: Connection):
+    c.config.sudo.password = os.getenv("SUDO_PASS")
+    output = c.sudo("tail /var/log/nginx/error.log")
+    print(output.stdout)
+
+
+@task
+def access(c: Connection):
+    c.config.sudo.password = os.getenv("SUDO_PASS")
+    output = c.sudo("tail /var/log/nginx/access.log")
+    print(output.stdout)
